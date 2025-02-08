d5b4d829435e92f71121c34c2bf8f71b2d5f9df0
karrybit <takumi.k.5610@gmail.com>
2025-02-08 15:39:05 +0900

    update

--- util/navigation.zsh
+++ util/navigation.zsh
@@ -98,3 +98,11 @@ function vimg() {
 
     vim "$dir"
 }
+
+function fzf-select-history() {
+    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N fzf-select-history
+bindkey '^r' fzf-select-history
