d6d7a1ebbb1f8189f40b40a2f50954bb1bcae7ea
karrybit <takumi.k.5610@gmail.com>
2025-06-26 22:06:23 +0900

    Remove memory usage module and isolate update_all error handling
    - Remove memory_usage module from starship config
    - Wrap update_all function body in subshell to limit scope of set -eo pipefail

--- util/package_manager.zsh
+++ util/package_manager.zsh
@@ -1,25 +1,28 @@
 #!/bin/zsh
 
 function update_all() {
-    set -eo pipefail
+    # Use subshell to limit scope of set -eo pipefail
+    (
+        set -eo pipefail
 
-    # for mac
-    pushd ~/dotfiles/homebrew
-    brew bundle dump -vf
-    brew upgrade
-    brew autoremove
-    brew cleanup
-    popd
+        # for mac
+        pushd ~/dotfiles/homebrew
+        brew bundle dump -vf
+        brew upgrade
+        brew autoremove
+        brew cleanup
+        popd
 
-    # for Arch
-    # yay -Syu
-    # yay -Yc
+        # for Arch
+        # yay -Syu
+        # yay -Yc
 
-    $DOTFILES_PATH/go/install.sh
-    $DOTFILES_PATH/rust/install.sh
-    gcloud components update
+        $DOTFILES_PATH/go/install.sh
+        $DOTFILES_PATH/rust/install.sh
+        gcloud components update
 
-    for repo in $(ghq list); do
-        ghq get --update --parallel "$repo"
-    done
+        for repo in $(ghq list); do
+            ghq get --update --parallel "$repo"
+        done
+    )
 }
